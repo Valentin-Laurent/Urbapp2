c908a5311a1008b3e70780de14df820c777e003c
Valentin Laurent <valentin_laurent@msn.com>
2016-03-16 23:41:32 +0100

    Barre de menu, non cliquable pour l'instant

--- app/src/main/java/fr/turfu/urbapp2/MainActivity.java
+++ app/src/main/java/fr/turfu/urbapp2/MainActivity.java
@@ -1,7 +1,10 @@
 package fr.turfu.urbapp2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
+
+    }
+
+
+    /**
+     * Method to inflate the xml menu file
+     * @param menu the menu
+     * @return true if everything went good
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+
+        //On sérialise le fichier menu.xml pour l'afficher dans la barre de menu
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
