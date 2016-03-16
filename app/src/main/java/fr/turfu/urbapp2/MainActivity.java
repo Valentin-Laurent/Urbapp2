24ad82486ad6d25f06a8b187aaf48c2d030a582c
FelixTx <felix.tournoux@gmail.com>
2016-03-16 23:54:38 +0100

    first step for db on mainactivity

--- app/src/main/java/fr/turfu/urbapp2/MainActivity.java
+++ app/src/main/java/fr/turfu/urbapp2/MainActivity.java
@@ -3,11 +3,22 @@ package fr.turfu.urbapp2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import fr.turfu.urbapp2.R;
+import fr.turfu.urbapp2.db.LocalDataSource;
+
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Attribut representing the local database
+     */
+    public static LocalDataSource datasource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        datasource = new LocalDataSource(this);
+
     }
 }
