fc8e987cbba5a873f139609255325d77c3508e6e
Valentin Laurent <valentin_laurent@msn.com>
2016-03-17 00:21:06 +0100

    Barre de menu désormais cliquable

--- app/src/main/java/fr/turfu/urbapp2/MainActivity.java
+++ app/src/main/java/fr/turfu/urbapp2/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,20 +19,45 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     /**
      * Method to inflate the xml menu file
+     *
      * @param menu the menu
      * @return true if everything went good
      */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
 
-        //On sérialise le fichier menu.xml pour l'afficher dans la barre de menu
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * Method to handle the clicks on the items of the toolbar
+     *
+     * @param item the item
+     * @return true if everything went good
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                return true;
+
+            case R.id.virtual_reality:
+                return true;
+
+            case R.id.settings:
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
