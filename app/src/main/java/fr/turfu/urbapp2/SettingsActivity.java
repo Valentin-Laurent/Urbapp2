4bb7c27f9998af135fc56cbe21249211cb41e1ba
Valentin Laurent <valentin_laurent@msn.com>
2016-03-17 18:15:01 +0100

    Ajout de la toolbar dans settings.

--- app/src/main/java/fr/turfu/urbapp2/SettingsActivity.java
+++ app/src/main/java/fr/turfu/urbapp2/SettingsActivity.java
@@ -1,7 +1,16 @@
 package fr.turfu.urbapp2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +18,64 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        //Handling toolbar
+        Toolbar mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
+        ActionBar menu = getSupportActionBar();
+        menu.setTitle(R.string.settings);
+    }
+
+    /**
+     * Method to inflate the xml menu file
+     * @param menu the menu
+     * @return true if everything went good
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+
+        //On sérialise le fichier menu.xml pour l'afficher dans la barre de menu
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * Method to handle the clicks on the items of the toolbar
+     *
+     * @param item the item
+     * @return true if everything went good
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.virtual_reality:
+                return true;
+
+            case R.id.settings:
+                return true;
+
+            case R.id.connectedAs:
+                Context context = getApplicationContext();
+                CharSequence text = getString(R.string.dialogClickOnConnectedAs);
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.setGravity(Gravity.CENTER, 0, 0); //To show the toast in the center of the screen
+                toast.show();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
