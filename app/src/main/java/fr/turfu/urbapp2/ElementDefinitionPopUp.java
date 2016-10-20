d7bf8b596ffa175f2c85accca0d54d201ee86e3a
LauraGuillot <lguillot@eleves.ec-nantes.fr>
2016-10-20 19:11:54 +0200

    Terminaison de la pop up pour caractériser une zone

--- app/src/main/java/fr/turfu/urbapp2/ElementDefinitionPopUp.java
+++ app/src/main/java/fr/turfu/urbapp2/ElementDefinitionPopUp.java
@@ -3,6 +3,7 @@ package fr.turfu.urbapp2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -10,6 +11,9 @@ import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import fr.turfu.urbapp2.DB.Element;
+import fr.turfu.urbapp2.Tools.ColorPicker;
 
 /**
  * Created by Laura on 10/10/2016.
@@ -70,7 +74,7 @@ public class ElementDefinitionPopUp extends Activity {
         Spinner spin = (Spinner) findViewById(R.id.MaterialValue);
         spin.setAdapter(adapter);
 
-        List elemType = new ArrayList();
+        List<String> elemType = new ArrayList();
         elemType.add("Sol");
         elemType.add("Mur");
         elemType.add("Toit");
@@ -86,8 +90,90 @@ public class ElementDefinitionPopUp extends Activity {
 
         //Affichage des informations de la zone
 
+        //On cherche l'élément du pixelGeom dans la liste
+        Element e = null;
+
+        for (Element el : ElementDefinitionActivity.elements) {
+            if (el.getPixelGeom_id() == pixelGeom_id) {
+                e = el;
+            }
+        }
+        for (Element el : ElementDefinitionActivity.newElements) {
+            if (el.getPixelGeom_id() == pixelGeom_id) {
+                e = el;
+            }
+        }
+
+        if (e != null) {
+            //Affichage du type de surface
+            spin1.setSelection((int) (e.getElementType_id() ));
+            //Affichage du matériau
+            spin.setSelection((int) e.getMaterial_id());
+            //Affichage de la couleur
+            ColorPicker colorPicker = (ColorPicker) findViewById(R.id.colorPicker);
+            colorPicker.setColor(Integer.parseInt(e.getElement_color()));
+        }
 
         //Boutons
+        no = (Button) findViewById(R.id.btn_cancel);
+        yes = (Button) findViewById(R.id.btn_ok);
+
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                finish();
+            }
+        });
+
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Element e = null;
+
+                for (Element el : ElementDefinitionActivity.elements) {
+                    if (el.getPixelGeom_id() == pixelGeom_id) {
+                        e = el;
+                        ElementDefinitionActivity.elements.remove(e);
+                    }
+                }
+                for (Element el : ElementDefinitionActivity.newElements) {
+                    if (el.getPixelGeom_id() == pixelGeom_id) {
+                        e = el;
+                        ElementDefinitionActivity.newElements.remove(e);
+                    }
+                }
+
+                if (e != null) {
+                    Spinner spin = (Spinner) findViewById(R.id.MaterialValue);
+                    e.setMaterial_id(spin.getSelectedItemId());
+
+                    Spinner spin1 = (Spinner) findViewById(R.id.TypeValue);
+                    e.setElementType_id(spin1.getSelectedItemId());
+
+                    ColorPicker colorPicker = (ColorPicker) findViewById(R.id.colorPicker);
+                    e.setElement_color(colorPicker.getColor() + "");
+                } else {
+                    e = new Element();
+
+                    Spinner spin = (Spinner) findViewById(R.id.MaterialValue);
+                    e.setMaterial_id(spin.getSelectedItemId());
+
+                    Spinner spin1 = (Spinner) findViewById(R.id.TypeValue);
+                    e.setElementType_id(spin1.getSelectedItemId());
+
+                    ColorPicker colorPicker = (ColorPicker) findViewById(R.id.colorPicker);
+                    e.setElement_color(colorPicker.getColor() + "");
+
+                    e.setPixelGeom_id(pixelGeom_id);
+                    e.setPhoto_id(photo_id);
+                    e.setElement_id(1 + ElementDefinitionActivity.elements.size() + ElementDefinitionActivity.newElements.size());
+                }
+
+                ElementDefinitionActivity.newElements.add(e);
+                finish();
+            }
+        });
+
 
     }
 
